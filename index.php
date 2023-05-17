f8abe2622f0425ee6e23b604c2e6becfc70cf961
AbdullahFal <abdullh_eyad@hotmail.com>
2023-05-17 15:21:56 +0300

    backend completed

--- index.php
+++ index.php
@@ -1,37 +1,61 @@
 <?php 
 include './inc/db.php';
 include './inc/form.php';
-
-//ret = retrieve.
-//for retrieving data from the DB
-$ret = 'SELECT * FROM users';
-$result = mysqli_query($conn, $ret);
-//fetch to show the result as a mysql query.
-$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-echo '<pre>';
-print_r($users);
-echo '</pre>';
-
-//always include at the end to close and reset(can include in seperate file)
-mysqli_free_result($result);
-mysqli_close($conn);
+include './inc/select.php';
+include './inc/db_close.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pau</title>
-</head>
-<body>
-    <form action="index.php" method="POST">
-        <input type="text" name="firstName" id="firstName" placeholder="">
-        <input type="text" name="lastName" id="lastName" placeholder="">
-        <input type="text" name="email" id="email" placeholder="">
-        <input type="submit" name="submit" value="send">
-    </form>
-</body>
-</html>
+<?php include_once './parts/header.php'; ?>
+    
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+            <div class="col-md-5 p-lg-5 mx-auto my-5">
+                <h1 class="display-4 fw-normal">VR/AR Medical Treatment</h1>
+                <p class="lead fw-normal">We are researching how to use VR technology and brain hijacking as a non-pharmacological treatment</p>
+                <a class="btn btn-outline-secondary" href="#">Find out more!</a>
+            </div>
+        </div>
+
+        <!-- <ul class="list-group list-group-flush">
+            <li class="list-group-item">An item</li>
+            <li class="list-group-item">A second item</li>
+            <li class="list-group-item">A third item</li>
+        </ul> -->
+
+        <div class="position-relative text-center">
+            <div class="col-md-6 p-lg-6 mx-auto my-6">
+                <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+                <h3>Please enter the following information</h3>
+
+                <div class="mb-3">
+                    <input type="text" name="firstName" class="form-control" id="firstName" placeholder="First Name" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text error"><?php echo $errors['firstNameError'] ?></div> <!--recalling errors from form.php from the array-->
+                </div>
+                
+                <div class="mb-3">
+                    <input type="text" name="lastName" class="form-control" id="lastName" placeholder="Last Name" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text error"><?php echo $errors['lastNameError'] ?></div>
+                </div>
+
+                <div class="mb-3">
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Email adress" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text error"><?php echo $errors['emailError'] ?></div>
+                </div>
+                
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    
+    <div class="row mb-5 pb-5">
+        <?php foreach($users as $user) : ?>
+            <div class="col-m-6">
+                <div class="card my-2 bg-light">
+                    <div class="card-body">    
+                    <h5 class="card-title"><?php echo htmlspecialchars($user['firstName']) . ' ' . htmlspecialchars($user['lastName']) ?></h5>
+                    <p class="card-text"><?php echo htmlspecialchars($user['email'])?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <?php include './parts/footer.php'; ?>
