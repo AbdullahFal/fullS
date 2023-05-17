9c970c0788d41f94ff1d6fd308c26aa37a21b1c4
AbdullahFal <abdullh_eyad@hotmail.com>
2023-05-17 21:34:39 +0300

    Final version

--- index.php
+++ index.php
@@ -6,21 +6,31 @@ include './inc/db_close.php';
 ?>
 
 <?php include_once './parts/header.php'; ?>
+
     
         <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-            <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <div class="col-md-5 p-lg-5 mx-auto">
+              <img src="image/t.png" alt="">
                 <h1 class="display-4 fw-normal">VR/AR Medical Treatment</h1>
                 <p class="lead fw-normal">We are researching how to use VR technology and brain hijacking as a non-pharmacological treatment</p>
-                <a class="btn btn-outline-secondary" href="#">Find out more!</a>
+                    <h3 id="#count-down"></h3>
+                
             </div>
-        </div>
 
-        <!-- <ul class="list-group list-group-flush">
+            <div class="container">
+            <ul class="list-group list-group-flush">
             <li class="list-group-item">An item</li>
             <li class="list-group-item">A second item</li>
             <li class="list-group-item">A third item</li>
-        </ul> -->
+        </ul> 
+            </div>
+        
+        </div>
 
+         
+
+        <div class="container">
+    <h3>To enter follow the following            </h3>
         <div class="position-relative text-center">
             <div class="col-md-6 p-lg-6 mx-auto my-6">
                 <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
@@ -46,16 +56,44 @@ include './inc/db_close.php';
             </div>
         </div>
     
-    <div class="row mb-5 pb-5">
-        <?php foreach($users as $user) : ?>
-            <div class="col-m-6">
-                <div class="card my-2 bg-light">
-                    <div class="card-body">    
-                    <h5 class="card-title"><?php echo htmlspecialchars($user['firstName']) . ' ' . htmlspecialchars($user['lastName']) ?></h5>
-                    <p class="card-text"><?php echo htmlspecialchars($user['email'])?></p>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach; ?>
+        <div class="loader-con">
+        <div id="loader">
+	<canvas id="circularLoader" width="200" height="200"></canvas>
+        </div>
+        </div>
+  
+
+        <!-- Button trigger modal -->
+        <div class = "d-grid gap-2 col-6 mx-auto my-5">
+<button  type="button" id = "winner" class="btn btn-primary">
+pick winner
+</button>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+
+      <h5 class="modal-title" id="modalLabel">the winner is</h5>
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <?php foreach($users as $user) : ?>
+        <h1 class=" display-3 text-center modal-title" id="modalLabel"><?php echo htmlspecialchars($user['firstName']) . ' ' . htmlspecialchars($user['lastName']) ?></h1>
+      <?php endforeach; ?>
+      </div>
+    
     </div>
+  </div>
+</div>
+
+
+
+
+    
     <?php include './parts/footer.php'; ?>
